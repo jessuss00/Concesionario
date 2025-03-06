3c32739d28bbd150479898659605fa6c723c252e
jessuss00 <jesusmadro23@gmail.com>
2025-03-06 14:36:38 +0100

    popin

--- src/vehiculo/camion/Camion.java
+++ src/vehiculo/camion/Camion.java
@@ -1,15 +1,24 @@
 package vehiculo.camion;
 
+
 import vehiculo.Vehiculo;
 
 public class Camion extends Vehiculo {
 	private int capacidadCarga;
+	private int[] paquetes = new int[11];
+
 	
 	public Camion(String marca, String modelo, int matricula,int capacidadCarga) {
 		super(marca, modelo, matricula);
 		this.capacidadCarga=capacidadCarga;
 	}
-	public void cargar() {}
+	public void cargar(int nPaquetes) {
+		if (nPaquetes==0) {
+			System.out.println("El camion esta vacio");
+		}
+		
+		
+	}
 	
 
 	public int getCapacidadCarga() {
--- src/vehiculo/ciclomotor/Main.java
+++ src/vehiculo/ciclomotor/Main.java
@@ -3,11 +3,7 @@ package vehiculo.ciclomotor;
 public class Main {
 
 	public static void main(String[] args) {
-
-		Moto m1 = new Moto("zip");
-
-		m1.bajarCaballito();
-
+		
 	}
 
 }
