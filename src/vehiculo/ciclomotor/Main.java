3f6cdfc9505ef22b141c5ce72d450431b954d41c
javierRuiz <fruijim102@g.educaand.es>
2025-03-06 14:05:59 +0100

    caballito

---
+++ src/vehiculo/ciclomotor/Main.java
@@ -0,0 +1,13 @@
+package vehiculo.ciclomotor;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Moto m1 = new Moto("zip");
+
+		m1.bajarCaballito();
+
+	}
+
+}
--- src/vehiculo/ciclomotor/Quad.java
+++ src/vehiculo/ciclomotor/Quad.java
@@ -1,5 +1,29 @@
 package vehiculo.ciclomotor;
 
 public class Quad extends Ciclomotor {
+
 	private String tamaño;
+	private boolean ruedaDelanteraElevada;
+
+	@Override
+	public void subirCaballito() {
+		if (ruedaDelanteraElevada == false) {
+			System.out.println("La moto acaba de empezar a hacer el caballito");
+			ruedaDelanteraElevada = true;
+		} else {
+			System.out.println("La moto ya esta haciendo el caballito");
+		}
+	}
+
+	@Override
+	public void bajarCaballito() {
+		if (ruedaDelanteraElevada == true) {
+			System.out.println("La moto ha dejado de hacer el caballito");
+			ruedaDelanteraElevada = false;
+		} else {
+			System.out.println("La moto no esta haciendo el caballito");
+
+		}
+	}
+
 }
